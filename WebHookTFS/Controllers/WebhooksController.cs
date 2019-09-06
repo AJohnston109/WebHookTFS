d53637f9753fb9f10954ede2b4bbc8190b298864
Anthony <A.Johnston@nditech.com>
2019-09-06 15:06:41 +0100

    Controller

--- WebHookTFS/Controllers/WebhooksController.cs
+++ WebHookTFS/Controllers/WebhooksController.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
 namespace WebHookTFS.Controllers
 {
     public class WebhooksController : Controller
+    {
+        public HttpProductInfoHeaderValueCollection UserAgent { get; }
+        public object RequestConstants { get; private set; }
+        // GET: Webhooks
+        public string GetReleases(string url)
+        {
+
+            var client = new WebClient();
+            var UserAgent = new HttpClient();
+            //client.Headers.Add(RequestConstants.UserAgent, RequestConstants.UserAgentValue);
+            var response = client.DownloadString(url);
+            return response;
+        }
+        private static readonly HttpClient httpClient;
+
+
+
+
+
+        public ActionResult Index()
+        {
+            WebHook.GetProjects();
+            return View();
+
+        }
+
+    }
+
+    public class HttpProductInfoHeaderValueCollection
     {
     }
 }
---
+++ WebHookTFS/Create_Update_Get.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHookTFS
+{
+    public class Create_Update_Get
+    {
+    }
+}
